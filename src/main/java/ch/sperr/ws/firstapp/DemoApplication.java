7a35f2682637209d1f24bbfdde0b025dc7b05263
sperr <christian@sperrzone.ch>
2018-09-12 12:29:39 +0200

    added some comment

--- src/main/java/ch/sperr/ws/firstapp/DemoApplication.java
+++ src/main/java/ch/sperr/ws/firstapp/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	//some comments to make a change
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
