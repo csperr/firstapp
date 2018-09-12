30e097d5e760c3f3de6e52428faf9176361f2cc1
sperr <christian@sperrzone.ch>
2018-09-12 12:02:50 +0200

    Initial import

---
+++ src/main/java/ch/sperr/ws/firstapp/DemoApplication.java
@@ -0,0 +1,12 @@
+package ch.sperr.ws.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
